3253c17862c322cdf76673aa90e0b17b749e89b1
JennaMurphree <158506727+JennaMurphree@users.noreply.github.com>
2024-11-18 18:38:50 -0700

    Update main.c

--- project/src/main.c
+++ project/src/main.c
@@ -3,3 +3,14 @@
 #include "led.h"
 #include "buzzer.h"
 #include "switches.h"
+
+int main(void) {
+  configureClocks(); /* setup master oscillator, CPU and peripharel clocks */
+  ledInit();
+  buzzerInit();
+  switchInit();
+  enableWDTInterrupts();
+
+  or_sr(0x18); /* CPU off, GIE on*/
+  
+}
