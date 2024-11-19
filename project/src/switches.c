ca400c4aa7f3415989a52a783e3871bd3cb2a2ca
JennaMurphree <158506727+JennaMurphree@users.noreply.github.com>
2024-11-18 18:02:51 -0700

    Update switches.c

--- project/src/switches.c
+++ project/src/switches.c
@@ -3,6 +3,14 @@
 #include "interuptVectorHandler.h"
 
 
+void switch_init(){
+  P2REN |= SWITCHES;  //enable resistors
+  P2IE |= SWITCHES;   // enable switch interrupts
+  P2OUT |= SWITCHES;  // pull ups for switches
+  P2DIR &= ~SWITCHES; // set swtich bits for input
+  switchPressCheck(); //set values that are currently being pressed.  Unsure if necessary 
+}
+
 void switchInterruptHandler(){
   char p2val = switchPressCheck(); //get the values of the switches that have been pressed
 }
