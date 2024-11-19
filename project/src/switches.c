007ca4071e2e71014e27f31610ba0733819a3142
JennaMurphree <158506727+JennaMurphree@users.noreply.github.com>
2024-11-18 17:58:22 -0700

    Update switches.c

--- project/src/switches.c
+++ project/src/switches.c
@@ -1,1 +1,11 @@
 #include <msp430.h>
+#include "switches.h"
+#include "led.h"
+#include "interuptVectorHandler.h"
+
+
+void switchInterruptHandler(){
+  char p2val = switchPressCheck(); //get the values of the switches that have been pressed
+}
+
+
