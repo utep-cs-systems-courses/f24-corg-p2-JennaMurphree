c5e4568b5793e51b22bcc98da08e4e248534fcd6
JennaMurphree <158506727+JennaMurphree@users.noreply.github.com>
2024-11-18 18:20:57 -0700

    Update led.c

--- project/src/led.c
+++ project/src/led.c
@@ -1,1 +1,13 @@
 #include <msp430.h>
+#include "led.h"
+
+
+void ledInit(){
+  P1DIR |= LEDS; // mark LEDS values as outputs
+  ledsOff(); 
+}
+
+void ledsOff(){
+  //initialize LED outputs to off  
+  P1OUT &= ~LEDS;
+}
